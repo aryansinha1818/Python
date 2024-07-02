38ce18848c38cf5d2660e97ea8809db9f1a60a93
Aryan <aryansinha1818@gmail.com>
2024-07-02 17:17:33 +0530

    changes made

--- Conceptual Programs/18. Dictionary.py
+++ Conceptual Programs/18. Dictionary.py
@@ -3,4 +3,8 @@ print(col["name"])
 print(col.get('age'))
 print(col.get('name'))
 print(col)
+# would print the values not the keys
 print(col.values())
+# only keys 
+print(col.keys())
+print(col.items())
