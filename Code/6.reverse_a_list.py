7104a0ddc9f02daab4177d2a99be6c0c780da5f6
Aryan Sinha <aryansinha18@Aryans-MacBook-Air.local>
2026-03-13 16:40:51 +0530

    file2

---
+++ Code/6.reverse_a_list.py
@@ -0,0 +1,19 @@
+l1 = [10,35, 45, 90,9,33]
+
+# l2 = []
+
+# len(l1), end before -1, step backward
+# for i in range(len(l1)-1, -1, -1):
+#     l2.append(l1[i])
+
+# start, end, back/reverse
+# l2 = l1[::-1]
+
+# l2 = list(reversed(l1))
+
+# print(l2)
+
+l1.reverse()
+
+print(l1)
+
